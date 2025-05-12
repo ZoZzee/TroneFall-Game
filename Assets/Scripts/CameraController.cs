084f7921936f0f00380f40ffd402a811ab3a49b4
ZoZzee <romamoch9@gmail.com>
2025-05-12 11:18:11 -0500

    0000

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float speed;
+
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, speed );
+        transform.position = smoothedPosition;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Transform cameraTransform;
+
+    public float moveSpeed;
+    public float speedRotation;
+    public Vector3 forvard;
+    public Vector3 right;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        forvard = cameraTransform.forward;
+        right = cameraTransform.right;
+
+        forvard.y = 0;
+        right.y = 0;
+
+
+        forvard.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = (forvard * vertical + right * horizontal).normalized;
+
+
+        if(moveDirection.magnitude > 0.1f)
+        {
+            rb.MovePosition(rb.position + moveDirection * moveSpeed);
+
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            rb.rotation = Quaternion.Slerp(rb.rotation,targetRotation,speedRotation);
+        }
+    }
+}
