5bec0bbf705b35efe4b2454a91c526872e3f7fdb
ZoZzee <romamoch9@gmail.com>
2025-10-31 03:07:41 -0500

    Перший рівень

--- Assets/Scripts/Buildings/BuildingPlan.cs
+++ Assets/Scripts/Buildings/BuildingPlan.cs
@@ -128,4 +128,10 @@ public class BuildingPlan : MonoBehaviour
             yield return null;
         }
     }
+    private void OnCollisionExit(Collision collision)
+    {
+        
+            Debug.Log("Collider");
+        
+    }
 }
